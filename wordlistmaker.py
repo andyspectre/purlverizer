33afe7c375c73385704433a64bd4a9269e1ad430
andyspectre <andyspectre@proton.me>
2023-02-22 18:54:31 +0100

    working basic script

--- wordlistmaker.py
+++ wordlistmaker.py
@@ -1,12 +1,51 @@
 import argparse
 import xml.etree.ElementTree as ET
+from pathlib import Path
+from urllib.parse import unquote, urlparse
 
 
-def read_file():
-    with open("test", encoding="utf 8") as f:
+def print_result(wordlist):
+    # print(wordlist)
+    for k, v in wordlist.items():
+        print("--------")
+        print(k, ":", len(v))
+        print("--------")
+        for i in v:
+            print(i)
+
+
+def get_files(filename):
+    with open(filename, encoding="utf 8") as f:
         tree = ET.parse(f)
-        for item in tree.findall("item"):
-            print(item.findtext("url"))
+        files = []
+        for tag in tree.iter():
+
+            # get file from url
+            if tag.tag == "url":
+                words = urlparse(tag.text)
+                if Path(words.path).suffix:
+                    if (
+                        unquote(Path(words.path).name) not in files
+                        and unquote(Path(words.path).name) != ""
+                    ):
+                        files.append(unquote(Path(words.path).name))
+        return files
+
+
+def get_folders(filename):
+    with open(filename, encoding="utf 8") as f:
+        tree = ET.parse(f)
+        folders = []
+        for tag in tree.iter():
+
+            # split urls based on '/' to get folders
+            if tag.tag == "url":
+                words = urlparse(tag.text).path.split("/")
+                for word in words:
+                    if word not in folders and word != "" and "." not in word:
+                        folders.append(word)
+            folders.sort()
+        return folders
 
 
 def start_cli_parser():
@@ -15,16 +54,29 @@ def start_cli_parser():
     )
     parser.add_argument("filename", type=str, help="A saved burpsuite xml file.")
     parser.add_argument(
-        "-f", "--folders", help="Print observed folders names.", action="store_true"
+        "-F", "--folders", help="Print observed folders names.", action="store_true"
+    )
+    parser.add_argument(
+        "-f", "--files", help="Print observed filenames.", action="store_true"
     )
     return parser
 
 
 def wordstree():
+    wordlist = dict()
+    folders = []
+    files = []
     parser = start_cli_parser()
     args = vars(parser.parse_args())
-    # myfile = read_file(args["filename"])
+    if args["folders"]:
+        folders = get_folders(args["filename"])
+    if args["files"]:
+        files = get_files(args["filename"])
+
+    wordlist["folders"] = folders
+    wordlist["files"] = files
+    print_result(wordlist)
 
 
 if __name__ == "__main__":
-    read_file()
+    wordstree()
