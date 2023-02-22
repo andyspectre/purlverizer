6b978f8e593080cf4ad56569a15e9954afd6de54
andyspectre <andyspectre@proton.me>
2023-02-23 00:40:55 +0100

    add get_files, get_folders functions and some error handling

--- wordlistmaker.py
+++ wordlistmaker.py
@@ -1,4 +1,6 @@
 import argparse
+import errno
+import sys
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from urllib.parse import unquote, urlparse
@@ -15,37 +17,48 @@ def print_result(wordlist):
 
 
 def get_files(filename):
-    with open(filename, encoding="utf 8") as f:
-        tree = ET.parse(f)
-        files = []
-        for tag in tree.iter():
+    try:
+        with open(filename, encoding="utf 8") as f:
+            tree = ET.parse(f)
+            files = []
+            for tag in tree.iter():
 
-            # get file from url
-            if tag.tag == "url":
-                words = urlparse(tag.text)
-                if Path(words.path).suffix:
-                    if (
-                        unquote(Path(words.path).name) not in files
-                        and unquote(Path(words.path).name) != ""
-                    ):
-                        files.append(unquote(Path(words.path).name))
+                # get file from url
+                if tag.tag == "url":
+                    words = urlparse(tag.text)
+                    if Path(words.path).suffix:
+                        if (
+                            unquote(Path(words.path).name) not in files
+                            and unquote(Path(words.path).name) != ""
+                        ):
+                            files.append(unquote(Path(words.path).name))
+    except UnicodeDecodeError:
+        sys.exit("Can't decode the content of the file.")
+    except ET.ParseError:
+        return
         return files
 
 
 def get_folders(filename):
-    with open(filename, encoding="utf 8") as f:
-        tree = ET.parse(f)
-        folders = []
-        for tag in tree.iter():
+    try:
+        with open(filename, encoding="utf 8") as f:
+            tree = ET.parse(f)
+            folders = []
+            for tag in tree.iter():
 
-            # split urls based on '/' to get folders
-            if tag.tag == "url":
-                words = urlparse(tag.text).path.split("/")
-                for word in words:
-                    if word not in folders and word != "" and "." not in word:
-                        folders.append(word)
-            folders.sort()
+                # split urls based on '/' to get folders
+                if tag.tag == "url":
+                    words = urlparse(tag.text).path.split("/")
+                    for word in words:
+                        if word not in folders and word != "" and "." not in word:
+                            folders.append(word)
+                folders.sort()
+    except UnicodeDecodeError:
+        sys.exit("Can't decode the content of the file.")
+    except ET.ParseError:
+        return
         return folders
+
 
 def split_path(urls):
     folders = []
@@ -57,6 +70,7 @@ def split_path(urls):
     folders.sort()
     return folders
 
+
 def read_file(filename):
     list_of_urls = []
     with open(filename, encoding="utf 8") as f:
@@ -64,6 +78,7 @@ def read_file(filename):
             url = line.strip()
             list_of_urls.append(url)
     return list_of_urls
+
 
 def start_cli_parser():
     parser = argparse.ArgumentParser(
@@ -86,23 +101,24 @@ def wordstree():
     parser = start_cli_parser()
     args = vars(parser.parse_args())
     try:
-        print("Parsing ", args["filename"],"...")
+        print("Parsing ", args["filename"], "...")
         if args["folders"]:
             folders = get_folders(args["filename"])
         if args["files"]:
             files = get_files(args["filename"])
-    except ET.ParseError:
-        pass
+    except FileNotFoundError:
+        sys.exit("No such file or directory.")
+
     try:
         list_of_urls = read_file(args["filename"])
         if args["folders"]:
             folders = split_path(list_of_urls)
-    except:
-        print("error")
-
-    wordlist["folders"] = folders
-    wordlist["files"] = files
-    print_result(wordlist)
+    except UnicodeDecodeError:
+        sys.exit("Can't decode the content of the doc file.")
+    else:
+        wordlist["folders"] = folders
+        wordlist["files"] = files
+        print_result(wordlist)
 
 
 if __name__ == "__main__":
