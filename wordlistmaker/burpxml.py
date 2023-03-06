185559ef5ce02ee157787eb356e5de43b8b6fb05
andyspectre <andyspectre@proton.me>
2023-03-06 01:53:33 +0100

    improve burpxml.py

--- wordlistmaker/burpxml.py
+++ wordlistmaker/burpxml.py
@@ -1,9 +1,42 @@
 import base64
+import re
 import tracemalloc
 import urllist
+from urllib.parse import unquote, urlparse
 import xml.etree.ElementTree as ET
 
 tracemalloc.start()
+
+regex_str = r"""
+  (?:"|')                               # Start newline delimiter
+  (
+    ((?:[a-zA-Z]{1,10}://|//)           # Match a scheme [a-Z]*1-10 or //
+    [^"'/]{1,}\.                        # Match a domainname (any character + dot)
+    [a-zA-Z]{2,}[^"']{0,})              # The domainextension and/or path
+    |
+    ((?:/|\.\./|\./)                    # Start with /,../,./
+    [^"'><,;| *()(%%$^/\\\[\]]          # Next character can't be...
+    [^"'><,;|()]{1,})                   # Rest of the characters can't be
+    |
+    ([a-zA-Z0-9_\-/]{1,}/               # Relative endpoint with /
+    [a-zA-Z0-9_\-/]{1,}                 # Resource name
+    \.(?:[a-zA-Z]{1,4}|action)          # Rest + extension (length 1-4 or action)
+    (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
+    |
+    ([a-zA-Z0-9_\-/]{1,}/               # REST API (no extension) with /
+    [a-zA-Z0-9_\-/]{3,}                 # Proper REST endpoints usually have 3+ chars
+    (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
+    |
+    ([a-zA-Z0-9_\-]{1,}                 # filename
+    \.(?:php|asp|aspx|jsp|json|
+         action|html|js|txt|xml)        # . + extension
+    (?:[\?|#][^"|']{0,}|))              # ? or # mark with parameters
+  )
+  (?:"|')                               # End newline delimiter
+"""
+
+regex = re.compile(regex_str, re.VERBOSE)
+
 
 def get_directories(burpfile):
     """
@@ -16,14 +49,30 @@ def get_directories(burpfile):
     directories_list = []
     n = 0
     try:
-        for event, elem in ET.iterparse(burpfile, events=("start","end")):
-            if event == "start":
-                continue
-            if event == "end" and elem.tag == "url":
+        for event, elem in ET.iterparse(burpfile):
+            if elem.tag == "url":
                 if elem.text not in url_list:
                     url_list.append(elem.text)
-                
-            if event == "end" and elem.tag == "item":
+            if elem.tag == "method":
+                if elem.text == "GET":
+                    continue
+
+            if elem.tag == "response":
+                if elem.attrib["base64"] == "true":
+                    print(base64.b64decode(elem.text))
+                else:
+                    elem.text = unquote(elem.text)
+
+                    print(elem.text)
+                    # urls = regex.search(elem.text)
+                    
+                    # for url in urls:
+                    #     if url not in url_list:
+                    #         url_list.append(url)
+                    # for url in url_list:
+                    #     print(url)
+
+            if elem.tag == "item":
                 n += 1
             elem.clear()
         print("Reached the end", n, "times.")
