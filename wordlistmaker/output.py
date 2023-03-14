cfc21c377c73eacc3056237f984559e6dba2c8ef
andyspectre <andyspectre@proton.me>
2023-03-14 21:40:26 +0100

    changes

--- wordlistmaker/output.py
+++
@@ -1,9 +0,0 @@
-
-def print_result(wordlist):
-        # print(wordlist)
-    for k, v in wordlist.items():
-        print("--------")
-        print(k, ":", len(v))
-        print("--------")
-        for i in v:
-            print(i)
